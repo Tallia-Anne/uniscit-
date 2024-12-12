53bd30a3d20df10f794aceb7418d45f977c765ef
Tallia-Anne <erinawambiekele@gmail.com>
2024-12-13 00:52:39 +0100

    mission

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -8,7 +8,11 @@ return (
     <div className={aboutcss.AboutCard}>
         <div className={aboutcss.About_Card_content}>
             <h1>Notre mission  est </h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam aut eligendi sit?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus fuga reiciendis iste?</p>
+            <p>Nous œuvrons pour une meilleure sensibilisation aux risques numériques en abordant des thèmes essentiels tels que le cyberharcèlement et la sécurité en ligne, afin de former les jeunes à une utilisation plus responsable et sécurisée du numérique.
+
+À travers des ateliers d’initiation au codage, nous introduisons les bases du développement informatique en utilisant des outils accessibles comme Scratch, encourageant ainsi leur créativité numérique.
+
+Enfin, nous proposons des formations sur l’utilisation constructive du numérique, permettant aux jeunes de mieux comprendre et maîtriser les outils digitaux pour les utiliser de manière efficace et positive.</p>
             <button>About us <i className='ri-arrow-right-up-line'></i></button>
         </div>
         <div className={aboutcss.About_Card_image}>
