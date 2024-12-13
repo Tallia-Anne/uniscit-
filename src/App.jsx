67fe1972e0d31bb9606e3afbc96c73ff50907c61
Tallia-Anne <erinawambiekele@gmail.com>
2024-12-13 14:29:02 +0100

    images

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
-
-import { BrowserRouter as Router, Routes,Route } from "react-router-dom"
-import Nav from "./Components/Nav/Nav"
-import Home from "./pages/Home/Home"
-import Atelier from "./pages/Atelier/Atelier"
-import Footer from "./Components/Footer/Footer"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./Components/Nav/Nav";
+import Home from "./pages/Home/Home";
+import Atelier from "./pages/Atelier/Atelier";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
- 
-
   return (
-    <>
-    <Nav/>
     <Router>
-    <Routes>
-      <Route path="/" exact element={<Home />} />
-     <Route path="/atelier" element={<Atelier />} />
-
+      <Nav />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/atelier" element={<Atelier />} />
       </Routes>
+      <Footer />
     </Router>
-    <Footer/>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,47 +1,43 @@
-import { useRef } from 'react';
+import { useRef } from 'react'; 
 import navCSS from './../Nav/Nav.module.css';
 import { Link } from "react-router-dom";
 
 function Nav() {
- 
   const menu = useRef();
 
   const menuHandler = () => {
-  menu.current.classList.toggle(navCSS.ShowMenu);
-}
-
+    if (menu.current) {
+      menu.current.classList.toggle(navCSS.ShowMenu);
+    }
+  }
 
   return (
     <div className={navCSS.nav_wrapper}>
-   <div className={navCSS.logo}>
-    <i className="ri-arrow-up-down-line"></i>
-    <a href="#">Unis-cité</a>
-  </div>
+      <div className={navCSS.logo}>
+        <i className="ri-arrow-up-down-line"></i>
+        <a href="#">Unis-cité</a>
+      </div>
 
-  <ul ref={menu}>
-    <li><a href="#home">Home</a></li>
-     <li><Link href="#about">About</Link></li>
-       <li><a href="/atelier" >Projects</a></li>
-       <li><a href="#features">Features</a></li>
-       <li><a href="#gallery">Gallery</a></li>
-       <li><a href="#testimonials">Testimonials</a></li>
-  </ul>
+      <ul ref={menu}>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><Link to="/atelier">Projects</Link></li>
+        <li><a href="#features">Features</a></li>
+        <li><a href="#gallery">Gallery</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+      </ul>
 
+      <div className={navCSS.Nav_btns}>
+        <div className={navCSS.social}>
+          <a><i className="ri-facebook-circle-fill"></i></a>
+          <a href="https://www.instagram.com/uniscite95/"><i className="ri-instagram-fill"></i></a>
+          <a href=""><i className="ri-twitter-x-line"></i></a>
+        </div>
 
-    <div className={navCSS.Nav_btns}>
-
-    <div className={navCSS.social}>
-    <a><i className="ri-facebook-circle-fill"></i></a>
-    <a href="https://www.instagram.com/uniscite95/"><i className="ri-instagram-fill"></i></a>
-    <a href=""><i className="ri-twitter-x-line"></i></a>
+        <i className="ri-menu-4-line" id={navCSS.bars} onClick={menuHandler}></i>
+      </div>
     </div>
-
-    <i className="ri-menu-4-line" id={navCSS.bars} onClick={menuHandler} ></i>
-
-    </div>
-
-</div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
